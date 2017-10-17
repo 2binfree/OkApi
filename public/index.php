d1145027d85de56f57eaa0e5d51e845a8182f882
Laurent Aubertin <2binfree@gmail.com>
2017-10-17 13:39:44 +0200

    add token authentification method

--- public/index.php
+++ public/index.php
@@ -4,10 +4,9 @@ namespace Wcs;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-$app = Application::getInstance();
-
+$app = Application::getInstance(Application::AUTH_METHOD_TOKEN);
 $github = new Github($app);
 
-var_dump($github->getUserInfos());
+var_dump($github->getUserInfos()['data']);
 
 
--- src/Application.php
+++ src/Application.php
@@ -19,32 +19,44 @@ namespace Wcs;
 
 class Application
 {
+    const AUTH_METHOD_BASIC = 1;
+    const AUTH_METHOD_TOKEN = 2;
+
     /** @var  Application */
     private static $instance;
 
     /** @var resource */
     private $client;
 
+    /** @var  string */
     private $login;
 
-    private function __construct()
+    /** @var  @var int */
+    private $authMethod;
+
+    private function __construct($authMethod = self::AUTH_METHOD_BASIC)
     {
         $credentials = include(__DIR__ . "/../config/credential.php");
+        $this->setAuthMethod($authMethod);
         $login = $credentials["username"];
         $password = $credentials["password"];
+        $token = $credentials["token"];
         $this->login = $login;
         $this->client = curl_init();
+        if ($this->authMethod === self::AUTH_METHOD_BASIC) {
+            curl_setopt($this->client, CURLOPT_USERPWD, "$login:$password");
+            curl_setopt($this->client, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        } elseif ($this->authMethod === self::AUTH_METHOD_TOKEN) {
+            curl_setopt($this->client, CURLOPT_HTTPHEADER, ["Authorization: token $token"]);
+        }
         curl_setopt($this->client, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($this->client, CURLOPT_USERPWD, "$login:$password");
-        curl_setopt($this->client, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($this->client, CURLOPT_HEADER, 1);
         curl_setopt($this->client, CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
     }
 
-    public static function getInstance()
+    public static function getInstance($authMethod = self::AUTH_METHOD_BASIC)
     {
         if (!is_a(self::$instance, 'Wcs\Application')){
-            self::$instance = new Application();
+            self::$instance = new Application($authMethod);
         }
         return self::$instance;
     }
@@ -70,6 +82,16 @@ class Application
     public function getLogin()
     {
         return $this->login;
+    }
+
+    /**
+     * @param int $method
+     * @return $this
+     */
+    public function setAuthMethod($method)
+    {
+        $this->authMethod = $method;
+        return $this;
     }
 
     public function __destruct()
